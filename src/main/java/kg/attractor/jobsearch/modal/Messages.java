dc6655f0867535ec70f78f04950dfec262ac677a
Adis <priv4484@gmail.com>
2025-03-14 14:55:50 +0600

    added models for the messages

---
+++ src/main/java/kg/attractor/jobsearch/modal/Messages.java
@@ -0,0 +1,10 @@
+package kg.attractor.jobsearch.modal;
+
+import java.sql.Timestamp;
+
+public class Messages {
+    private int id;
+    private int respondedApplicants;
+    private String content;
+    private Timestamp timestamp;
+}
