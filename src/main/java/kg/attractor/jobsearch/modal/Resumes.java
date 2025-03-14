06f606c8d53b1a9059b9d09e06afe0b51a39386a
Adis <priv4484@gmail.com>
2025-03-14 14:55:27 +0600

    added models for the resumes

---
+++ src/main/java/kg/attractor/jobsearch/modal/Resumes.java
@@ -0,0 +1,14 @@
+package kg.attractor.jobsearch.modal;
+
+import java.sql.Timestamp;
+
+public class Resumes {
+    private int id;
+    private int applicantId;
+    private String name;
+    private int categoryId;
+    private double salary;
+    private boolean isActive;
+    private Timestamp createdDate;
+    private Timestamp updateTime;
+}
