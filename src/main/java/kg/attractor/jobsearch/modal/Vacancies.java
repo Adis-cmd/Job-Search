107f2c3c3f6bf03c5beb6bf7b784c545f8fa2a50
Adis <priv4484@gmail.com>
2025-03-14 14:54:48 +0600

    added models for the vacancy

---
+++ src/main/java/kg/attractor/jobsearch/modal/Vacancies.java
@@ -0,0 +1,18 @@
+package kg.attractor.jobsearch.modal;
+
+import java.security.Timestamp;
+
+public class Vacancies {
+    private int id;
+    private String description;
+    private int categoryId;
+    private double salary;
+    private int expFrom;
+    private int expTo;
+    private boolean isActive;
+    private int authorId;
+    private Timestamp createdDate;
+    private Timestamp updatedTime;
+
+
+}
