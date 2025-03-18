706881396714da8ee5c6d8624bab6a8ab1ab51fc
Adis <priv4484@gmail.com>
2025-03-18 18:20:28 +0600

    added the fileutils class and methods for searching for photos and uploading

---
+++ src/main/java/kg/attractor/jobsearch/util/FileUtil.java
@@ -0,0 +1,92 @@
+package kg.attractor.jobsearch.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import lombok.SneakyThrows;
+import org.apache.catalina.User;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class FileUtil {
+
+    private final Gson gson;
+
+    private static final String UPLOAD_DIR = "data/";
+
+    public FileUtil() {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+
+    public List<User> getUser(String path) {
+        Type listType = new TypeToken<Map<String, List<User>>>() {
+        }.getType();
+        try (Reader reader = new FileReader(UPLOAD_DIR + path)) {
+            Map<String, List<User>> user = gson.fromJson(reader, listType);
+            return user.get("user");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return List.of();
+        }
+    }
+
+
+    @SneakyThrows
+    public String saveUploadFile(MultipartFile file, String subDir) {
+        String uuidFile = UUID.randomUUID().toString();
+        String resultFileName = uuidFile + "_" + file.getOriginalFilename();
+
+        Path pathDir = Paths.get(UPLOAD_DIR + subDir);
+        Files.createDirectories(pathDir);
+
+        Path filePath = Paths.get(pathDir + "/" + resultFileName);
+        if (!Files.exists(filePath)) {
+            Files.createFile(filePath);
+        }
+        try (OutputStream os = Files.newOutputStream(filePath)) {
+            os.write(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return resultFileName;
+    }
+
+    @SneakyThrows
+    public ResponseEntity<?> getOutputFile(String filename, String subDir, MediaType mediaType) {
+        try {
+            byte[] image = Files.readAllBytes(Paths.get(UPLOAD_DIR + subDir + filename));
+            Resource resource = new ByteArrayResource(image);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename=\"" + filename + "\"")
+                    .contentLength(resource.contentLength())
+                    .contentType(mediaType)
+                    .body(resource);
+        } catch (NoSuchFileException e) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Image not found");
+        }
+    }
+
+}
