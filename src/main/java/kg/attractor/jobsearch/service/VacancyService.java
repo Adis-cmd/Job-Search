74678feba25e8bade5e9736ce79f6c9b3e719802
Adis <priv4484@gmail.com>
2025-03-25 18:54:37 +0600

    added logic for the vacancy

--- src/main/java/kg/attractor/jobsearch/service/VacancyService.java
+++ src/main/java/kg/attractor/jobsearch/service/VacancyService.java
@@ -6,17 +6,17 @@ import kg.attractor.jobsearch.modal.Vacancy;
 import java.util.List;
 
 public interface VacancyService {
-    void editVacancies(VacancyDto vacanciesDto, int vacancyId);
+    void editVacancies(VacancyDto vacanciesDto, Long vacancyId);
 
-    void createVacancies(VacancyDto vacanciesDto);
+    void createVacancies(VacancyDto vacanciesDto, Long authorId);
 
     List<VacancyDto> getVacancies();
 
-    void deleteVacancies(Integer vacancyId, Vacancy vacancies);
+    void deleteVacancies(Long vacancyId);
 
     List<VacancyDto> getAllVacanciesCategory(Long categoryId);
 
     List<VacancyDto> getAllVacancyByResponded();
 
-    List<Vacancy> getAllVacanciesIsActive(Vacancy vacancies);
+    List<VacancyDto> getAllVacanciesIsActive();
 }
