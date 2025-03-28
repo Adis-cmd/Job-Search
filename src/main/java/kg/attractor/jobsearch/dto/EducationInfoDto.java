82352be9c7503acca038f1eeb55325e69a25f531
Adis <priv4484@gmail.com>
2025-03-28 06:56:42 +0600

    Annotations have been added to validate education data.

--- src/main/java/kg/attractor/jobsearch/dto/EducationInfoDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/EducationInfoDto.java
@@ -1,5 +1,9 @@
 package kg.attractor.jobsearch.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,10 +14,20 @@ import java.time.LocalDate;
 @Builder
 public class EducationInfoDto {
     private Long id;
+    @NotNull(message = "id резюме не должен быть пустым")
     private Long resumeId;
+    @NotBlank(message = "Учебное заведение обязательно для заполнения")
+    @Size(max = 100, message = "Название учебного заведения не должно превышать 100 символов")
     private String institution;
+    @NotBlank(message = "Название программы обязательно")
+    @Size(max = 100, message = "Название программы не должно превышать 100 символов")
     private String program;
+    @NotNull(message = "Дата начала обучения обязательна")
+    @PastOrPresent(message = "Дата начала должна быть в прошлом или настоящем")
     private LocalDate startDate;
+    @PastOrPresent(message = "Дата окончания должна быть в прошлом или настоящем")
     private LocalDate endDate;
+    @NotBlank(message = "Степень обязательна")
+    @Size(max = 50, message = "Название степени не должно превышать 50 символов")
     private String degree;
 }
