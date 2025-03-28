e986dc0873f9ce16733c49ccd11a5b94f3f91fac
Adis <priv4484@gmail.com>
2025-03-28 06:48:00 +0600

    added logic for searching for data from the database in the education info table

--- src/main/java/kg/attractor/jobsearch/service/EducationInfoService.java
+++ src/main/java/kg/attractor/jobsearch/service/EducationInfoService.java
@@ -2,6 +2,12 @@ package kg.attractor.jobsearch.service;
 
 import kg.attractor.jobsearch.dto.EducationInfoDto;
 
+import java.util.List;
+
 public interface EducationInfoService {
+    List<EducationInfoDto> getAllEducationInfo();
+
+    EducationInfoDto getEducationInfoById(Long educationInfoId);
+
     void createEducationInfo(EducationInfoDto educationInfoDto);
 }
