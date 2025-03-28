95afd7011ed8b1894685423cabd133daa3753d5c
Adis <priv4484@gmail.com>
2025-03-28 06:49:11 +0600

    implemented methods in the interface

--- src/main/java/kg/attractor/jobsearch/service/WorkExperienceInfoService.java
+++ src/main/java/kg/attractor/jobsearch/service/WorkExperienceInfoService.java
@@ -1,4 +1,13 @@
 package kg.attractor.jobsearch.service;
 
+import kg.attractor.jobsearch.dto.WorkExperienceInfoDto;
+
+import java.util.List;
+
 public interface WorkExperienceInfoService {
+    WorkExperienceInfoDto getWorkExperienceInfoById(Long workId);
+
+    List<WorkExperienceInfoDto> getAllWorkExperienceInfo();
+
+    void createWorkExperienceInfo(WorkExperienceInfoDto wDto);
 }
