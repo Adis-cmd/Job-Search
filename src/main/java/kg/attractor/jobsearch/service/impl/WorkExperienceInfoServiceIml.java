b8c2d9586af1e855de9064581bcad03d98f72349
Adis <priv4484@gmail.com>
2025-03-28 06:45:42 +0600

    added all the business logic to output data from the database from work experience

--- src/main/java/kg/attractor/jobsearch/service/impl/WorkExperienceInfoServiceIml.java
+++ src/main/java/kg/attractor/jobsearch/service/impl/WorkExperienceInfoServiceIml.java
@@ -2,16 +2,52 @@ package kg.attractor.jobsearch.service.impl;
 
 import kg.attractor.jobsearch.dao.WorkExperienceInfoDao;
 import kg.attractor.jobsearch.dto.WorkExperienceInfoDto;
+import kg.attractor.jobsearch.exception.WorkExperienceInfoException;
 import kg.attractor.jobsearch.modal.WorkExperienceInfo;
 import kg.attractor.jobsearch.service.WorkExperienceInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WorkExperienceInfoServiceIml implements WorkExperienceInfoService {
     private final WorkExperienceInfoDao workExperienceInfoDao;
 
+    @Override
+    public WorkExperienceInfoDto getWorkExperienceInfoById(Long workId) {
+        WorkExperienceInfo work = workExperienceInfoDao.getWorkExperienceInfoById(workId)
+                .orElseThrow(WorkExperienceInfoException::new);
+
+
+        return WorkExperienceInfoDto.builder()
+                .id(work.getId())
+                .resumeId(work.getResumeId())
+                .years(work.getYears())
+                .companyName(work.getCompanyName())
+                .position(work.getPosition())
+                .responsibilities(work.getResponsibilities())
+                .build();
+    }
+
+    @Override
+    public List<WorkExperienceInfoDto> getAllWorkExperienceInfo() {
+        List<WorkExperienceInfo> weid = workExperienceInfoDao.getAllWorkExperienceInfo();
+
+        return weid.stream()
+                .map(w -> WorkExperienceInfoDto.builder()
+                        .id(w.getId())
+                        .resumeId(w.getResumeId())
+                        .years(w.getYears())
+                        .companyName(w.getCompanyName())
+                        .position(w.getPosition())
+                        .responsibilities(w.getResponsibilities())
+                        .build())
+                .toList();
+    }
+
+    @Override
     public void createWorkExperienceInfo(WorkExperienceInfoDto wDto) {
         WorkExperienceInfo w = new WorkExperienceInfo();
         w.setResumeId(wDto.getResumeId());
