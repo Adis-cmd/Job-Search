a8f506142aa8acb52084d8c1ce66cde2c58c2bcd
Adis <priv4484@gmail.com>
2025-03-28 06:54:31 +0600

    Implemented the validation of resume data using annotations

--- src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
+++ src/main/java/kg/attractor/jobsearch/dto/ResumeDto.java
@@ -1,5 +1,6 @@
 package kg.attractor.jobsearch.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -13,9 +14,20 @@ import java.util.List;
 public class ResumeDto {
     private Long id;
     private Long applicantId;
+    @NotBlank(message = "Название резюме не может быть пустым")
+    @Size(min = 1, max = 100)
+    @Pattern(
+            regexp = "^[\\p{L}\\d .,!?—-]+$",
+            message = "Допустимы буквы, цифры, пробелы и знаки .,!?—-"
+    )
     private String name;
+    @NotNull(message = "Категория не может быть пустым")
     private Long categoryId;
+    @NotNull(message = "Зарплата обязательна для заполнения")
+    @DecimalMin(value = "100", message = "Зарплата не может быть меньше 100")
+    @DecimalMax(value = "1000000000.0", message = "Максимальная зарплата: 1 000 000 000")
     private Double salary;
+    @NotNull(message = "Статус активности должен быть указан")
     private Boolean isActive;
     private LocalDateTime createdDate;
     private LocalDateTime updateTime;
