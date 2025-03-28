61a929b08e76547550d68abe9eb2755f1e92d2a9
Adis <priv4484@gmail.com>
2025-03-28 06:40:51 +0600

    Added methods for searching and displaying information about education

--- src/main/java/kg/attractor/jobsearch/controller/EducationInfoController.java
+++ src/main/java/kg/attractor/jobsearch/controller/EducationInfoController.java
@@ -1,11 +1,28 @@
 package kg.attractor.jobsearch.controller;
 
+import kg.attractor.jobsearch.dto.EducationInfoDto;
+import kg.attractor.jobsearch.modal.EducationInfo;
+import kg.attractor.jobsearch.service.EducationInfoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("educationInfo")
 @RequiredArgsConstructor
 public class EducationInfoController {
+    private final EducationInfoService educationInfoService;
+
+    @GetMapping("{educationInfoId}")
+    public EducationInfoDto getEducationIngoById(@PathVariable Long educationInfoId) {
+        return  educationInfoService.getEducationInfoById(educationInfoId);
+    }
+    @GetMapping
+    public List<EducationInfoDto> getAllEducationInfo(){
+        return educationInfoService.getAllEducationInfo();
+    }
 }
