813e93a940aeca9a4c823fb0919071f0d8d065ff
Adis <priv4484@gmail.com>
2025-03-28 06:44:03 +0600

    added queries to output data from the database

--- src/main/java/kg/attractor/jobsearch/dao/EducationInfoDao.java
+++ src/main/java/kg/attractor/jobsearch/dao/EducationInfoDao.java
@@ -1,24 +1,47 @@
 package kg.attractor.jobsearch.dao;
 
 import kg.attractor.jobsearch.modal.EducationInfo;
-import lombok.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class EducationInfoDao {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public List<EducationInfo> getAllEducationInfo() {
+        String sql = "select * from education_info";
+
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(EducationInfo.class));
+    }
+
+    public Optional<EducationInfo> getEducationInfoById(Long eduId) {
+        String sql = "select * from  education_info where id = ?";
+
+        return Optional.ofNullable(
+                DataAccessUtils.singleResult(
+                        jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(EducationInfo.class), eduId)
+                )
+        );
+    }
+
 
     public void createEducationInfo(EducationInfo educationInfo) {
         String sql = """
-
-
-insert into education_info 
-                (resumeId, institution, program, startDate, endDate, degree) 
-                 values (:resumeId, :institution, :program, :startDate, :endDate, :degree)
-                 """;
+                insert into education_info 
+                                (resumeId, institution, program, startDate, endDate, degree) 
+                                 values (:resumeId, :institution, :program, :startDate, :endDate, :degree)
+                """;
 
         namedParameterJdbcTemplate.update(
                 sql, new MapSqlParameterSource()
