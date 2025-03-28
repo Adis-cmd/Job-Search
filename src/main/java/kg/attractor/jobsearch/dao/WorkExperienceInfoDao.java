0b2b88d596e595381df09115a9f53820a953b6e1
Adis <priv4484@gmail.com>
2025-03-28 06:44:25 +0600

    added queries to output data from the database

--- src/main/java/kg/attractor/jobsearch/dao/WorkExperienceInfoDao.java
+++ src/main/java/kg/attractor/jobsearch/dao/WorkExperienceInfoDao.java
@@ -2,14 +2,35 @@ package kg.attractor.jobsearch.dao;
 
 import kg.attractor.jobsearch.modal.WorkExperienceInfo;
 import lombok.*;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class WorkExperienceInfoDao {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
+    public List<WorkExperienceInfo> getAllWorkExperienceInfo(){
+        String sql = "Select * from work_experience_info";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(WorkExperienceInfo.class));
+    }
+
+    public Optional<WorkExperienceInfo> getWorkExperienceInfoById(Long workId){
+        String sql = "select * from work_experience_info where id = ?";
+        return  Optional.ofNullable(
+                DataAccessUtils.singleResult(
+                        jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(WorkExperienceInfo.class), workId)
+                )
+        );
+
+    }
 
     public void createWorkExperienceInfo(WorkExperienceInfo workExperienceInfo) {
         String sql = "insert into work_experience_info " +
