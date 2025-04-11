ccde0d18b7d19bdff91a6f02551b024bb2a3a0e4
Adis <priv4484@gmail.com>
2025-04-11 17:20:25 +0600

    add resume controller

--- src/main/java/kg/attractor/jobsearch/controller/ResumeController.java
+++ src/main/java/kg/attractor/jobsearch/controller/ResumeController.java
@@ -1,58 +1,26 @@
 package kg.attractor.jobsearch.controller;
 
-import jakarta.validation.Valid;
 import kg.attractor.jobsearch.dto.ResumeDto;
-import kg.attractor.jobsearch.modal.Resume;
 import kg.attractor.jobsearch.service.ResumeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("resumes")
+@Controller
 @RequiredArgsConstructor
+@RequestMapping("resume")
 public class ResumeController {
-
-    private final ResumeService resumesService;
-
-    @GetMapping("search")
-    public List<ResumeDto> search() {
-        return resumesService.getAllResumes();
-    }
-
-    @PostMapping("add")
-    public HttpStatus addResumes(@Valid @RequestBody ResumeDto resumesDto, @RequestParam(name = "userId")  String userId) {
-        resumesService.createResumes(resumesDto,  userId);
-        return HttpStatus.CREATED;
-    }
-
-    @PutMapping("update/{resumeId}")
-    public HttpStatus updateResumes(@Valid @PathVariable("resumeId") String resumeId, @RequestBody ResumeDto resumesDto) {
-        resumesService.editResume(resumesDto, resumeId);
-        return HttpStatus.OK;
-    }
-
-    @DeleteMapping("delete/{resumeId}")
-    public HttpStatus deleteResumes(@PathVariable("resumeId") String resumeId) {
-        resumesService.deleteResumes(resumeId);
-        return HttpStatus.OK;
-    }
-
-    @GetMapping("search/{categoryId}")
-    public List<ResumeDto> searchResumesCategory(@PathVariable String categoryId) {
-        return resumesService.getResumeCategory(categoryId);
-    }
-
-    @GetMapping("user/{userId}")
-    public List<ResumeDto> getAllResumesByUserId(@PathVariable String userId) {
-        return resumesService.getResumeByUserid(userId);
-    }
+    private final ResumeService resumeService;
 
     @GetMapping
-    public ResumeDto getResumesById(@RequestParam(name = "id") String id) {
-        return resumesService.getResumeById(id);
+    public String getAllResume(Model model) {
+        List<ResumeDto> allResume = resumeService.getAllResumes();
+        model.addAttribute("resumes", allResume);
+        return "resume/allResume";
     }
 
 }
