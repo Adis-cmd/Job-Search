dbd16b4a85b2ffe87b70312e56b0db70c953a4b6
Adis <priv4484@gmail.com>
2025-04-11 17:21:32 +0600

    add vacancy controller

--- src/main/java/kg/attractor/jobsearch/controller/VacancyController.java
+++ src/main/java/kg/attractor/jobsearch/controller/VacancyController.java
@@ -1,62 +1,28 @@
 package kg.attractor.jobsearch.controller;
 
-import jakarta.validation.Valid;
 import kg.attractor.jobsearch.dto.VacancyDto;
+import kg.attractor.jobsearch.service.UserService;
 import kg.attractor.jobsearch.service.VacancyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("vacancies")
+@Controller
 @RequiredArgsConstructor
+@RequestMapping("vacancy")
 public class VacancyController {
-    private final VacancyService vacanciesService;
-
-    @GetMapping("search/{vacancyId}")
-    public VacancyDto getVacancyById(@PathVariable String vacancyId) {
-        return vacanciesService.getVacancyById(vacancyId);
-    }
-
-    @PostMapping("add")
-    public HttpStatus createVacancies(@RequestBody @Valid  VacancyDto vacanciesDto, @RequestParam(name = "authorId") String authorId) {
-        vacanciesService.createVacancies(vacanciesDto, authorId);
-        return HttpStatus.CREATED;
-    }
-
-    @PutMapping("update/{vacancyId}")
-    public HttpStatus editVacancies(@RequestBody @Valid @PathVariable("vacancyId") VacancyDto vacanciesDto, String vacancyId) {
-        vacanciesService.editVacancies(vacanciesDto, vacancyId);
-        return HttpStatus.OK;
-    }
-
-    @DeleteMapping("delete/{vacanciesId}")
-    public HttpStatus deleteVacancies(@PathVariable("vacanciesId") String vacancyId) {
-        vacanciesService.deleteVacancies(vacancyId);
-        return HttpStatus.OK;
-    }
+    private final VacancyService vacancyService;
+    private final UserService userService;
 
     @GetMapping
-    public List<VacancyDto> getVacancies() {
-        return vacanciesService.getVacancies();
+    public String getAllVacancies(Model model) {
+        List<VacancyDto> getAllVacancy = vacancyService.getVacancies();
+
+        model.addAttribute("vacancies", getAllVacancy);
+        return "vacancy/allVacancy";
     }
-
-    @GetMapping("search/category/{categoryId}")
-    public List<VacancyDto> allVacanciesCategory(@PathVariable("categoryId") String categoryId) {
-        return vacanciesService.getAllVacanciesCategory(categoryId);
-    }
-
-    @GetMapping("isActive")
-    public List<VacancyDto> allIsActiveVacancies() {
-        return vacanciesService.getAllVacanciesIsActive();
-    }
-
-    @GetMapping("response")
-    public List<VacancyDto> allVacanciesByResponse() {
-       return vacanciesService.getAllVacancyByResponded();
-    }
-
-
 }
